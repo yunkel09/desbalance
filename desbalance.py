5e5a931a3a7433ebf811a0081a4f73d5b6d87757
William Chavarría <yunkel09@outlook.com>
2022-09-06 21:30:04 -0600

    termine pipelines y tuning

--- desbalance.py
+++ desbalance.py
@@ -2,40 +2,55 @@
 # _____________________________________________________________________________
 # módulos                                                                  ####
 
+# .............................................................................
+# usuales                                                                  ####
+
 import pandas as pd
 import seaborn as sns
-import imblearn as im
+# import imblearn as im
 import matplotlib.pyplot as plt
 
+# .............................................................................
+# sklearn                                                                  ####
+
+from sklearn.model_selection import (train_test_split as tts,
+                                     RepeatedStratifiedKFold,
+                                     RandomizedSearchCV)
+
+# preprocesamiento
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, RobustScaler
+
+# modelos
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split as tts
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import RepeatedStratifiedKFold as rskf
-from imblearn.over_sampling import SMOTE
-from imblearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, RobustScaler
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from xgboost import XGBClassifier
+
+# métricas
 from sklearn.metrics import classification_report
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from scipy.stats import loguniform
+
+# .............................................................................
+# tuberías                                                                 ####
+
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.compose import ColumnTransformer, make_column_selector
+from imblearn.pipeline import Pipeline as imbPipeline
+
+# .............................................................................
+# muestreo                                                                 ####
+
+from imblearn.over_sampling import SMOTE
+from imblearn.combine import SMOTEENN, SMOTETomek
+from imblearn.under_sampling import RandomUnderSampler
 
 # _____________________________________________________________________________
 # carga                                                                    ####
 
 df = pd.read_csv('interior.csv')
 df
-
-# _____________________________________________________________________________
-# preparación                                                              ####
-
-# categóricos
-cat_var = ['zona', 'bu']
-df[cat_var] = (df[cat_var].
-               apply(lambda x: pd.Series(x).
-               astype('category')))
-
-# numéricos
-num_var = df.select_dtypes(exclude='category').columns.tolist()
-num_var.remove('interior')
-df.info()
 
 # _____________________________________________________________________________
 # exploración                                                              ####
@@ -52,8 +67,6 @@ plt.show()
 
 X = df.drop('interior', axis=1)
 y = df.interior
-
-# train(70%), validación (15%), test (15%)
 
 # aplicar estratificación
 X_train, X_test, y_train, y_test = tts(X,
@@ -75,110 +88,265 @@ te = round(len(X_test) / len(df), 3)
 print(f'trai: {tr}\nvali: {va}\ntest: {te}')
 
 # _____________________________________________________________________________
-# preprocesamiento                                                         ####
-
-num_tr = Pipeline(steps=[
-    ('scale', RobustScaler())])
-
-cat_tr = Pipeline(steps=[
-    ('one-hot', OneHotEncoder(handle_unknown='infrequent_if_exist',
-                              sparse=False))])
-
-ct = ColumnTransformer(transformers=[
-    ('num', num_tr, num_var),
-    ('cat', cat_tr, cat_var)
-])
-
-# _____________________________________________________________________________
-# validación cruzada                                                       ####
-
-rep_stratified_kfold = rskf(n_splits=10,
-                            n_repeats=5,
-                            random_state=22)
-
-# _____________________________________________________________________________
 # estimadores                                                              ####
 
-logreg = LogisticRegression(random_state=22, max_iter=1000)
+# Linear model (logistic regression)
+lr = LogisticRegression(warm_start=True, solver='lbfgs', penalty='l2')
 
-# _____________________________________________________________________________
-# balanceadores                                                            ####
+# RandomForest
+rf = RandomForestClassifier()
 
-smt = SMOTE(random_state=22)
+# XGB
+xgb = XGBClassifier(tree_method="hist", verbosity=0, silent=True)
 
-# .............................................................................
-# evidenciar proceso de balanceo                                           ####
-
-# balancear
-new_df = X_train.drop(cat_var, axis=1)
-x_trainb, y_trainb = smt.fit_resample(new_df, y_train)
-df_balanceado = pd.DataFrame(y_trainb, columns=['interior'])
-
-# gráfico de barra balanceado
-sns.countplot(y='interior', data=df_balanceado, palette="Set2")
-plt.show()
+# Ensemble
+lr_xgb_rf = VotingClassifier(estimators=[('lr', lr),
+                                         ('xgb', xgb),
+                                         ('rf', rf)],
+                             voting='soft')
 
 # _____________________________________________________________________________
 # pipelines                                                                ####
 
-# con balanceo
-con_smt = im.pipeline.Pipeline([
-    ('preprocesador', ct),
-    ('smt', smt),
-    ('rel', logreg)
+# receta 1: sin balanceo
+sin_muestreo = imbPipeline([
+    # paso 1: aplicar transformaciones
+    ('prep', ColumnTransformer([
+        # paso 1.1: features numéricos
+        ('num', make_pipeline(
+             StandardScaler()),
+         make_column_selector(dtype_include='float64')
+        ),
+        # paso 1.2 features categóricos
+        ('cat', make_pipeline(
+            OneHotEncoder(sparse=False,
+                          handle_unknown='infrequent_if_exist')),
+         make_column_selector(dtype_include='category')
+        )])
+    ),
+    # paso 3: consolidar estimador (ensemble)
+    ('ensemble', lr_xgb_rf)
 ])
 
-# sin balanceo
-sin_smt = im.pipeline.Pipeline([
-    ('preprocesador', ct),
-    ('rel', logreg)
+# receta 2: solo smote
+standar_smote = imbPipeline([
+    # paso 1: aplicar transformaciones
+    ('prep', ColumnTransformer([
+        # paso 1.1: features numéricos
+        ('num', make_pipeline(
+             RobustScaler()),
+         make_column_selector(dtype_include='float64')
+        ),
+        # paso 1.2 features categóricos
+        ('cat',make_pipeline(
+            OneHotEncoder(sparse=False, 
+                          handle_unknown='infrequent_if_exist')),
+         make_column_selector(dtype_include='category')
+        )])
+    ),
+     # paso 3: muestreo
+    ('smote', SMOTE(random_state=22)),
+     # paso 4: consolidar estimador (ensemble)
+    ('ensemble', lr_xgb_rf)
 ])
 
+# receta 3: SMOTE and Edited Nearest Neighbours (https://bit.ly/3x2ED7E)
+nearest_neighbours = imbPipeline([
+    # paso 1: aplicar transformaciones
+    ('prep', ColumnTransformer([
+        # paso 1.1: features numéricos
+        ('num', make_pipeline(
+            StandardScaler()),
+         make_column_selector(dtype_include='float64')
+        ),
+        # paso 1.2 features categóricos
+        ('cat',make_pipeline(
+            OneHotEncoder(sparse=False, 
+                          handle_unknown='infrequent_if_exist')),
+         make_column_selector(dtype_include='category')
+        )])
+    ),
+    # paso 3: muestreo
+    ('smote', SMOTEENN(random_state=22)),
+    # paso 4: consolidar estimador (ensemble)
+    ('ensemble', lr_xgb_rf)
+])
+
+# receta 4: RandomUnderSampler
+under_sampler = imbPipeline([
+    # paso 1: aplicar transformaciones
+    ('prep', ColumnTransformer([
+        # paso 1.1: features numéricos
+        ('num', make_pipeline(
+            StandardScaler()),
+         make_column_selector(dtype_include='float64')
+        ),
+        # paso 1.2 features categóricos
+        ('cat',make_pipeline(
+            OneHotEncoder(sparse=False, 
+                          handle_unknown='infrequent_if_exist')),
+         make_column_selector(dtype_include='category')
+        )])
+    ),
+    # paso 3: muestreo
+    ('smote', RandomUnderSampler(random_state=22)),
+    # paso 4: consolidar estimador (ensemble)
+    ('ensemble', lr_xgb_rf)
+])
+
+# receta 5: sobre-muestreo con SMOTETomek
+smote_tomek = imbPipeline([
+    # paso 1: aplicar transformaciones
+    ('prep', ColumnTransformer([
+        # paso 1.1: features numéricos
+        ('num', make_pipeline(
+            StandardScaler()),
+         make_column_selector(dtype_include='float64')
+        ),
+        # paso 1.2 features categóricos
+        ('cat',make_pipeline(
+            OneHotEncoder(sparse=False, 
+                          handle_unknown='infrequent_if_exist')),
+         make_column_selector(dtype_include='category')
+        )])
+    ),
+    # paso 3: muestreo
+    ('smote', SMOTETomek(random_state=22)),
+    # paso 4: consolidar estimador (ensemble)
+    ('ensemble', lr_xgb_rf)
+])
+
+
+tuberias = [sin_muestreo,
+            standar_smote,
+            nearest_neighbours,
+            under_sampler,
+            smote_tomek]
+
+pipe_dict = {0: 'sin_balanceo',
+             1: 'smote_simple',
+             2: 'smoteenn',
+             3: 'sub_muestreo',
+             4: 'smotetomek'}
+
 # _____________________________________________________________________________
-# hiperparámetros                                                          ####
+# cuadrícula de hiperparámetros                                            ####
 
-param_grid = {'rel__C': [0.001, 0.01, 0.1, 1, 10, 100, 1000]}
+params = {
+    # 'ensemble__lr__solver': ['newton-cg', 'lbfgs', 'liblinear'],
+    # 'ensemble__lr__penalty': ['l2'],
+    'ensemble__lr__C': loguniform(1e-5, 100),
+    'ensemble__xgb__learning_rate': [0.1],
+    'ensemble__xgb__max_depth': [7, 10, 15, 20],
+    'ensemble__xgb__min_child_weight': [10, 15, 20, 25],
+    'ensemble__xgb__colsample_bytree': [0.8, 0.9, 1],
+    'ensemble__xgb__n_estimators': [300, 400, 500, 600],
+    'ensemble__xgb__reg_alpha': [0.5, 0.2, 1],
+    'ensemble__xgb__reg_lambda': [2, 3, 5],
+    'ensemble__xgb__gamma': [1, 2, 3],
+    'ensemble__rf__max_depth': [7, 10, 15, 20],
+    'ensemble__rf__min_samples_leaf': [1, 2, 4],
+    'ensemble__rf__min_samples_split': [2, 5, 10],
+    'ensemble__rf__n_estimators': [300, 400, 500, 600],
+}
 
 # _____________________________________________________________________________
-# cuadrícula                                                               ####
+# validación cruzada                                                       ####
 
-grid_search = GridSearchCV(estimator=con_smt,
-                           param_grid=param_grid,
-                           scoring='recall',
-                           error_score='raise',
-                           cv=rep_stratified_kfold,
-                           n_jobs=-1)
+rsf = RepeatedStratifiedKFold(n_splits=3,
+                              n_repeats=2,
+                              random_state=22)
+
+cdx = []
+for pipe in tuberias:
+    cdx.append(RandomizedSearchCV(estimator=pipe,
+                                  param_distributions=params,
+                                  scoring='recall',
+                                  error_score='raise',
+                                  verbose=2,
+                                  n_jobs=-1,
+                                  cv=rsf))
 
 # _____________________________________________________________________________
 # fit                                                                      ####
 
-grid_search.fit(X_train, y_train)
+# training time: 177 seg con 12 nucleos físicos en paralelo
+for pipe in cdx:
+    pipe.fit(X_train, y_train)
 
 # _____________________________________________________________________________
 # métricas                                                                 ####
 
+dxl = []
+for idx, val in enumerate(cdx):
+
+    y_pred = val.predict(X_val)
+
+    val_recall = recall_score(y_true=y_val,
+                              y_pred=y_pred,
+                              pos_label=1,
+                              zero_division=1,
+                              average='binary')
+
+    val_precision = precision_score(y_true=y_val,
+                                    y_pred=y_pred,
+                                    pos_label=1,
+                                    zero_division=1,
+                                    average='binary')
+
+    val_f1 = f1_score(y_true=y_val,
+                      y_pred=y_pred,
+                      pos_label=1,
+                      zero_division=1,
+                      average='binary')
+
+    dxl.append([pipe_dict[idx],
+                val.best_score_,
+                val_recall,
+                val_precision,
+                val_f1])
+
+
 # .............................................................................
-# entrenamiento                                                            ####
+# cv_score y validación                                                    ####
 
-cv_score = grid_search.best_score_
-print(f'Cross-validation recall score: {cv_score}')
+dfx = pd.DataFrame(dxl, columns=['modelo',
+                                 'cv-score',     # recall
+                                 'recall',
+                                 'precision',
+                                 'f1-score'])
+dfx
 
 # .............................................................................
-# validación                                                               ####
+# seleccionar mejor modelo                                                 ####
 
-y_pred = grid_search.predict(X_val)
-print(classification_report(y_val,
-                            y_pred,
-                            zero_division=1,
-                            target_names=['exterior', 'interior']))
+# smote-simple
+cdx[1]
 
 # .............................................................................
 # prueba                                                                   ####
 
-y_pred = grid_search.predict(X_test)
+prediccion = cdx[1].predict(X_test)
 print(classification_report(y_test,
-                            y_pred, 
-                            zero_division=1))
+                            prediccion,
+                            zero_division=1,
+                            target_names=['exterior', 'interior']))
+
+# .............................................................................
+# mejores hiperparámetros                                                  ####
+
+best_params = cdx[1].best_params_
+hiper = pd.DataFrame.from_dict(best_params, orient='index')
+hiper
+
+# _____________________________________________________________________________
+# gráfica                                                                  ####
+
+
+
+
 
 # _____________________________________________________________________________
 # End Of Script                                                            ####
+
+
